8b5d917d464e70231c5551950853dd37c8f6e3ae
Chris Franklin <cf73021@gmail.com>
2019-03-02 03:57:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function getRepositories() {
+  const name = document.getElementById("username").value
+  const uri = rootURL + "/users/" + name + "/repos"
   const req = new XMLHttpRequest();
   req.addEventListener("load", displayRepositories)
   req.open("GET", uri);
